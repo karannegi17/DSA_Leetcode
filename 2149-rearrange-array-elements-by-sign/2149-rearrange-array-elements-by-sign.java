78d3c581c64c6c64d907d799dae961e5c2bb694c
karannegi17 <karannegi829@gmail.com>
2026-08-06 11:05:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,16 +1,17 @@
 import java.util.Arrays;
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int[] arr = new int[nums.length];
+        int l = nums.length;
+        int[] arr = new int[l];
         int positive = 0;
         int negative = 1;
-        for(int num : nums){
+        for(int i = 0; i < l; i++){
             if(num > 0){
-                arr[positive] = num;
+                arr[positive] = nums[i];
                 positive += 2;
             }
             else{
-                arr[negative] = num;
+                arr[negative] = nums[i];
                 negative += 2;
             }
         }
